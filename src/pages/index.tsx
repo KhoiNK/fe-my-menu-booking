6bbcff7634fec8613d8728c6e4ec7987ee086dd3
KhoiNK <ng.kimkhoi2002@gmail.com>
2024-04-04 14:15:45 +0700

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Booking: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to my booking!</h1>
+      <p>This is a paragraph of text on the homepage.</p>
+    </div>
+  );
+};
+
+export default Booking;
